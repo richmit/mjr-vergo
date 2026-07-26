8b184a2fb89a09ff2c948e6272c720519dfcf85d
Mitch Richling <11151403+richmit@users.noreply.github.com>
2026-07-26 14:20:32 -0500

    Version update

--- mjr-vergo.el
+++ mjr-vergo.el
@@ -1,9 +1,25 @@
-;; mjr-vergo --- Provide verGo.sh in Emacs. -*-coding: utf-8 lexical-binding:t mode:emacs-lisp -*-
+;; mjr-vergo -*-coding: utf-8 lexical-binding:t; mode:emacs-lisp; fill-column:158 -*-
 
-;; Copyright (C) 2026-2026 First Last me@mitchr.me
+;; Copyright (c) 2026-2026 Mitch Richling <https://www.mitchr.me>.  All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+;;
+;; 1. Redistributions of source code must retain the above copyright notice, this list of conditions, and the following disclaimer.
+;;
+;; 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the documentation
+;;    and/or other materials provided with the distribution.
+;;
+;; 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without
+;;    specific prior written permission.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+;; FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+;; TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ;; Author:      Mitch Richling
-;; Version:     0.12
+;; Version:     0.14
 ;; Keywords:    verGo
 ;; URL:         https://github.com/richmit/mjr-vergo
 
