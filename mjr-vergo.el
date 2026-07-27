a0b55f18164607d656d2adcac268e3e6bce3c5b6
Mitch Richling <11151403+richmit@users.noreply.github.com>
2026-07-27 12:12:26 -0500

    header experiment

--- mjr-vergo.el
+++ mjr-vergo.el
@@ -1,4 +1,4 @@
-;; mjr-vergo.el --- verGo.sh wrapper. -*-coding: utf-8 lexical-binding:t; mode:emacs-lisp; fill-column:158 -*-
+;; mjr-vergo.el --- verGo.sh wrapper. -*- lexical-binding:t; coding: utf-8; mode:emacs-lisp; fill-column:158 -*-
 
 ;; Copyright (c) 2026-2026 Mitch Richling <https://www.mitchr.me>.  All rights reserved.
 ;;
@@ -19,7 +19,7 @@
 ;; TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ;; Author:      Mitch Richling
-;; Version:     0.17
+;; Version:     0.18
 ;; Keywords:    verGo
 ;; URL:         https://github.com/richmit/mjr-vergo
 
