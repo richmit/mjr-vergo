1f79052126eb5a65ab88ea8755a01d1207f5e4b7
Mitch Richling <11151403+richmit@users.noreply.github.com>
2026-07-31 15:32:58 -0500

    Bump version

--- el-vergo.el
+++ el-vergo.el
@@ -19,7 +19,7 @@
 ;; TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ;; Author:      Mitch Richling <https://www.mitchr.me>
-;; Version:     0.22
+;; Version:     0.25
 ;; Keywords:    verGo
 ;; URL:         https://github.com/richmit/el-vergo
 
